7182442b0bc23b9d22bc451afa5326e0aca79981
sebastian enciso <sebastianbabble@gmail.com>
2022-11-06 12:55:34 -0800

    events

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,5 +1,23 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-//another
+contract Web3RSVP{
+    //1. Event struct that contains all properties someone would need to create an event
+    // eventDataCID is a string url for all data stored offchain on IPFS
+    struct CreateEvent{
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
 
+    //2. Mapping for to index all events. idToEvent(321) = that certain event
+    mapping (bytes32 => CreateEvent) public idToEvent;
+    
+
+}
